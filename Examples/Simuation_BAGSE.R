81cffc6c873a8fede40d78ed636cba12d6339cb0
abhukku <32854014+abhukku@users.noreply.github.com>
2019-04-24 17:34:19 -0400

    Update Simuation_BAGSE.R

--- Examples/Simuation_BAGSE.R
+++ Examples/Simuation_BAGSE.R
@@ -44,8 +44,7 @@ t = torus_cpp(betahat,sebetahat,annotation)
 t$enrichment_est
 #Should be -3.07 for a0 and 0.93 for a1. 
 
-#If you want to compare the results to the R package, the R code is in the R folder in the repo
-source("bagse.R")
+#If you want to compare the results to the R package, the R code is in the R folder in the repo, function name is torus
 r = torus(betahat,sebetahat,annotation)
 r$enrichment_est
 #Should be -3.07 for a0 and 0.93 for a1 again, but markedly slower
