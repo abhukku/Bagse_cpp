e4857dec8c98444045d51deff12487d5caa0e756
abhukku <32854014+abhukku@users.noreply.github.com>
2019-04-23 23:49:58 -0400

    Update Simuation_BAGSE.R

--- Examples/Simuation_BAGSE.R
+++ Examples/Simuation_BAGSE.R
@@ -42,7 +42,7 @@ annotation = annot_vec
 
 t = torus_cpp(betahat,sebetahat,annotation)
 t$enrichment_est
-#Should be -3.06 for a0 and 0.93 for a1. 
+#Should be -3.07 for a0 and 0.93 for a1. 
 
 #If you want to compare the results to the R package, the R code is in the R folder in the repo
 source("bagse.R")
